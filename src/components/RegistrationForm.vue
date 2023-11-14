88b4b5811f8054a4e8d49de639658e3094267e5f
tranthanhtung96 <tttung1996@gmail.com>
2023-11-15 02:40:46 +0700

    fix: css

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -16,8 +16,8 @@ const submit = () => {
 </script>
 
 <template>
-  <div class="flex max-w-screen-xl mx-auto p-8">
-    <div class="flex items-center w-1/3 bg-cyan-500 p-8">
+  <div class="flex max-w-screen-xl mx-auto shadow-md">
+    <div class="flex items-center w-1/3 bg-cyan-500 p-8 rounded-l">
       <div class="border-l-4 border-cyan-500 pl-4">
         <p class="text-3xl font-bold text-white pb-4">KÍNH MỜI</p>
         <p class="text-xl text-white">Để tiện cho gia đình cô dâu và chú rể chuẩn bị tiệc cưới, mong anh/chị/bạn bớt
@@ -25,39 +25,39 @@ const submit = () => {
       </div>
     </div>
 
-    <form class="w-2/3 bg-white shadow-md rounded-md p-8">
-      <p class="text-3xl font-bold mb-8 text-gray-800">HAPPY FORM</p>
+    <form class="w-2/3 bg-white p-8 rounded-r">
+      <p class="text-3xl font-bold mb-4 text-gray-800">HAPPY FORM</p>
 
       <div class="mb-4">
-        <table class="w-full">
-          <tr class="flex items-center mb-4">
-            <th class="w-2/5 text-right pr-4 text-gray-800">Họ tên</th>
-            <th class="w-3/5">
+        <table class="w-full border-separate [border-spacing:0.75rem]">
+          <tr class="items-center">
+            <th class="w-30 text-right pr-4 text-gray-800">Họ tên</th>
+            <th>
               <input type="text" class="p-2 w-full border rounded-md text-gray-800 bg-gray-300" required
                      v-model="registration.name">
             </th>
           </tr>
 
-          <tr class="flex items-center mb-4">
-            <th class="w-2/5 text-right pr-4 text-gray-800">Số điện thoại</th>
-            <th class="w-3/5">
+          <tr class="items-center">
+            <th class="w-30 text-right pr-4 text-gray-800">Số điện thoại</th>
+            <th>
               <input type="text" class="p-2 w-full border rounded-md text-gray-800 bg-gray-300" required
                      v-model="registration.phoneNumber">
             </th>
           </tr>
 
-          <tr class="flex items-center mb-4">
-            <th class="w-2/5 text-right pr-4 text-gray-800">Đi mấy người</th>
-            <th class="w-3/5">
+          <tr class="items-center">
+            <th class="w-30 text-right pr-4 text-gray-800">Đi mấy người</th>
+            <th>
               <input type="number" id="participants" min="1" max="6"
                      class="p-2 w-full border rounded-md text-gray-800 bg-gray-300" required
                      v-model="registration.participants">
             </th>
           </tr>
 
-          <tr class="flex items-center mb-4">
-            <th class="w-2/5 text-right pr-4 text-gray-800">Bạn của</th>
-            <th class="w-3/5">
+          <tr class="items-center">
+            <th class="w-30 text-right pr-4 text-gray-800">Bạn của</th>
+            <th>
               <div class="flex items-center">
                 <input type="radio" id="bride" class="mr-2" v-model="registration.isBrideSGuests" v-bind:value="true">
                 <label for="bride" class="text-gray-800">Cô dâu</label>
@@ -69,9 +69,9 @@ const submit = () => {
             </th>
           </tr>
 
-          <tr class="flex items-center mb-4">
-            <th class="w-2/5 text-right pr-4 text-gray-800">Đi ô tô không</th>
-            <th class="w-3/5">
+          <tr class="items-center">
+            <th class="w-30 text-right pr-4 text-gray-800">Đi ô tô không</th>
+            <th>
               <div class="flex items-center">
                 <input type="radio" id="busYes" class="mr-2" required v-model="registration.isBusRegistered"
                        v-bind:value="true">
