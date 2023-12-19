0816e88fb8688050ad6e86261710b2bd0f5775b0
tranthanhtung96 <tttung1996@gmail.com>
2023-12-19 08:21:41 +0700

    fix: registration

--- src/components/RegistrationSection.vue
+++ src/components/RegistrationSection.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { ref } from "vue";
 import SectionHeader from "./SectionHeader.vue";
+import axios from "axios";
 
 const registration = ref({
   name: "",
   phoneNumber: "",
-  participants: 1,
-  isBrideSGuests: true,
+  count: 1,
+  bride: false,
+  groom: false,
 });
 
-const submit = () => {
-  console.log({ ...registration.value });
+const submit = async () => {
+  const resp = await axios.post(
+    // "https://shiny-melomakarona-dee082.netlify.app/.netlify/functions/register",
+    "http://localhost:8888/.netlify/functions/register",
+    registration.value,
+  );
+  console.log(resp)
 };
 </script>
 
 <template>
   <div
-    class="flex flex-col items-center justify-center bg-[url('/confetti.png')] bg-repeat-x py-16 md:py-24"
+    class="flex flex-col items-center justify-center bg-[#F2EDED] bg-opacity-50 py-16 md:py-24"
   >
     <SectionHeader title="Đăng ký xe">
       Nếu khoảng cách giữa đôi ta là 1000 bước, em chỉ cần bước một còn anh sẽ
@@ -28,8 +35,7 @@ const submit = () => {
       <div
         class="rounded-b-none rounded-t-xl bg-rose-300 p-8 text-xl text-white sm:w-1/3 sm:rounded-l-xl sm:rounded-r-none"
       >
-        Để tiện cho gia đình cô dâu và chú rể chuẩn bị tiệc cưới, mong
-        anh/chị/bạn bớt chút thời gian điền vào form này ạ
+        Mọi người muốn đăng ký xe từ Hà Nội về đám cưới thì điền form này ạ.
       </div>
 
       <div
@@ -41,7 +47,7 @@ const submit = () => {
             <th>
               <input
                 type="text"
-                class="w-full rounded-md border bg-gray-300 p-2 text-gray-800"
+                class="w-full rounded-md border-2 border-rose-300 p-2 text-gray-800"
                 required
                 v-model="registration.name"
               />
@@ -53,7 +59,7 @@ const submit = () => {
             <th>
               <input
                 type="tel"
-                class="w-full rounded-md border bg-gray-300 p-2 text-gray-800"
+                class="w-full rounded-md border-2 border-rose-300 p-2 text-gray-800"
                 required
                 v-model="registration.phoneNumber"
               />
@@ -61,48 +67,51 @@ const submit = () => {
           </tr>
 
           <tr class="items-center">
-            <th class="pr-4 text-right text-gray-800">Đi mấy người</th>
+            <th class="pr-4 text-right text-gray-800">Đăng ký mấy người</th>
             <th>
               <input
                 type="number"
-                id="participants"
                 min="1"
                 max="6"
-                class="w-full rounded-md border bg-gray-300 p-2 text-gray-800"
+                class="w-full rounded-md border-2 border-rose-300 p-2 text-gray-800"
                 required
-                v-model="registration.participants"
+                v-model="registration.count"
               />
             </th>
           </tr>
 
           <tr class="items-center">
-            <th class="pr-4 text-right text-gray-800">Bạn của</th>
+            <th class="pr-4 text-right text-gray-800">Đăng ký xe</th>
             <th>
-              <div class="flex items-center">
-                <input
-                  type="radio"
-                  id="bride"
-                  class="mr-2"
-                  v-model="registration.isBrideSGuests"
-                  v-bind:value="true"
-                />
-                <label for="bride" class="text-gray-800">Cô dâu</label>
+              <div class="flex flex-row items-center gap-2">
+                <div
+                  class="flex w-max basis-1/2 flex-row items-center rounded-md border-2 border-rose-300 px-2 py-1"
+                >
+                  <input
+                    type="checkbox"
+                    class="border-rose mr-3 h-4 w-4 accent-rose-300 ring-rose-300"
+                    v-model="registration.bride"
+                  />
+                  <div class="text-gray-800">Cô dâu</div>
+                </div>
 
-                <input
-                  type="radio"
-                  id="groom"
-                  class="ml-7 mr-2"
-                  v-model="registration.isBrideSGuests"
-                  v-bind:value="false"
-                />
-                <label for="groom" class="text-gray-800">Chú rể</label>
+                <div
+                  class="flex w-max basis-1/2 flex-row items-center rounded-md border-2 border-rose-300 px-2 py-1"
+                >
+                  <input
+                    type="checkbox"
+                    class="border-rose mr-3 h-4 w-4 accent-rose-300"
+                    v-model="registration.groom"
+                  />
+                  <div class="text-gray-800">Chú rể</div>
+                </div>
               </div>
             </th>
           </tr>
         </table>
         <button
-          class="w-3/4 rounded-md bg-rose-300 p-2 font-bold text-white hover:bg-cyan-800"
-          v-on:click.prevent="submit"
+          class="w-3/4 rounded-md bg-rose-300 p-2 font-bold text-white hover:bg-rose-600"
+          v-on:click="submit"
         >
           ĐĂNG KÝ
         </button>
